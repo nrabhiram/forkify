07fbec7e10349b3a36f86ee17fa54da4262df6c5
Abhiram Reddy <62073792+nrabhiram@users.noreply.github.com>
2020-05-16 19:45:15 +0530

    search model
    Created the data structure for the search model

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,35 @@
-// Global app controller
-import num from './test';
-const x = 23;
-console.log(`I imported ${num} from another module called test.js! Variable x is ${x}`);
+import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements } from './views/base';
+
+/** Global state of the app
+ * - Search oject
+ * - Current recipe object
+ * - Shopping list object
+ * - Like recipes
+ */
+const state = {}
+
+const controlSearch = async () => {
+    // 1. Get query from view
+    const query = 'pizza';//TODO
+
+    if (query) {
+        // 2. Create new search object and add to state
+        state.search = new Search(query);
+
+        // 3. Prepare UI for the results i.e spinner
+
+        // 4. Search for recipes
+        await state.search.getResults();
+
+        // 5. Render results on the UI
+        console.log(state.search.recipes);
+    }
+}
+
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
